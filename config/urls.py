bf8e845c6ddcfd6fbbaa7e674fcd75a0cc9fda20
jtorreggiani <joetorreggiani@gmail.com>
2024-09-25 15:00:03 -0400

    Add sentry.

--- config/urls.py
+++ config/urls.py
@@ -19,9 +19,13 @@ from django.contrib import admin
 from django.urls import include, path
 from web import views
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("db/", views.db, name="db"),
     path("admin/", admin.site.urls),
     path('django-rq/', include('django_rq.urls')),
+    path('sentry-debug/', trigger_error),
 ]
