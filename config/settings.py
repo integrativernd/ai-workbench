c66a0bff2661a5a74ea61999f09e887d7ca4bcab
jtorreggiani <joetorreggiani@gmail.com>
2024-09-25 14:54:29 -0400

    Add sentry init command.

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 import secrets
 from pathlib import Path
-
+import sentry_sdk
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -333,3 +333,14 @@ TOOL_DEFINITIONS = [
         }
     },
 ]
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_DSN'),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
