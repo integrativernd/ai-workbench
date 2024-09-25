d0a26d61828d8f62f7527e2cc21823dbad7ae3b4
jtorreggiani <joetorreggiani@gmail.com>
2024-09-24 20:21:53 -0400

    Rename hello to web.

--- web/apps.py
+++ web/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class HelloConfig(AppConfig):
+class WebConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "hello"
+    name = "web"
