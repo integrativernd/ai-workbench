d1c823ef196c4a5cc8baf6069f461c2cc04df05e
jtorreggiani <joetorreggiani@gmail.com>
2024-09-25 21:04:20 -0400

    Make google auth view admin only.

--- tools/views.py
+++ tools/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.conf import settings
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import user_passes_test
 from tools.google.docs import get_google_auth_url, handle_oauth2_callback
 
+def superuser_required(view_func):
+    decorated_view_func = user_passes_test(lambda u: u.is_superuser)(view_func)
+    return staff_member_required(decorated_view_func)
+
+@superuser_required
 def start_google_auth(request):
     authorization_url, state = get_google_auth_url()
     request.session['google_auth_state'] = state
     print(authorization_url)
     return redirect(authorization_url)
 
+@superuser_required
 def google_auth_callback(request):
     state = request.GET.get('state')
     code = request.GET.get('code')
