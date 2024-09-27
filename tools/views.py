47c7cd456574ff6953880f6103037b638df6c089
jtorreggiani <joetorreggiani@gmail.com>
2024-09-27 17:26:03 -0400

    Clean up google auth code.

--- tools/views.py
+++ tools/views.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.urls import reverse
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import user_passes_test
-from tools.google.docs import get_google_auth_url, handle_oauth2_callback
+from tools.google.auth import get_google_auth_url, handle_oauth2_callback
 
 def superuser_required(view_func):
     decorated_view_func = user_passes_test(lambda u: u.is_superuser)(view_func)
