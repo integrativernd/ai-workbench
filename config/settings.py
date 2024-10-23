f435dd38d63d6b984fad61519eb1a924f99c0387
jtorreggiani <joetorreggiani@gmail.com>
2024-10-22 20:50:36 -0400

    Fix CI issues.

--- config/settings.py
+++ config/settings.py
@@ -134,18 +134,15 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if PRODUCTION:
-    # In production on Heroku the database configuration is derived from the `DATABASE_URL`
-    # environment variable by the dj-database-url package. `DATABASE_URL` will be set
-    # automatically by Heroku when a database addon is attached to your Heroku app. See:
-    # https://devcenter.heroku.com/articles/provisioning-heroku-postgres#application-config-vars
-    # https://github.com/jazzband/dj-database-url
+if PRODUCTION or 'GITHUB_ACTIONS' in os.environ:
+    # In production on Heroku or GitHub Actions, the database configuration is derived from the `DATABASE_URL`
+    # environment variable by the dj-database-url package.
     DATABASES = {
         "default": dj_database_url.config(
             env="DATABASE_URL",
             conn_max_age=600,
             conn_health_checks=True,
-            ssl_require=True,
+            ssl_require=PRODUCTION,  # Only require SSL in production, not in GitHub Actions
         ),
     }
 else:
